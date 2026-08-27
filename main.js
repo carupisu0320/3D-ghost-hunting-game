59ec4fc1bf6e13f3c9327f4486ef8cf43180b2e5
carupisu0320 <nekohaoki215@gmail.com>
2026-08-27 13:02:18 +0900

    Refactor map imports and add new map functionality

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // このファイルはマップを増やしても書き換えなくて済むよう、薄いブートストラップだけにしてある。
-// 実際のゲームの仕組みは engine.js、この家の中身は house-map.js に分かれている
+// 実際のゲームの仕組みは engine.js、各マップの中身はそれぞれの map ファイルに分かれている。
+// マップの実体(build())は、選ばれるまでは呼ばない(2つ以上のマップを同時に組み立ててしまわないようにするため)
 import { addMapCard, startEngine, enterGame } from './engine.js';
-import { mapId, mapLabel } from './house-map.js'; // importした時点でマップの中身がまるごと構築される
+import { build as buildHouse, mapLabel as houseLabel } from './house-map.js';
+import { build as buildGrafton, mapLabel as graftonLabel } from './grafton-map.js';
 
-// マップ選択画面に登録する。今後マップが増えたら、同じ形でここに1行足すだけでよい
-addMapCard(mapLabel, true, enterGame);
-addMapCard('近日追加予定', false, null);
+addMapCard(houseLabel, true, () => { buildHouse(); enterGame(); });
+addMapCard(graftonLabel, true, () => { buildGrafton(); enterGame(); });
 addMapCard('近日追加予定', false, null);
 
 startEngine();
